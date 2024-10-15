9421beae07fe5d3d80a45ab0d13995b3552c9909
helmersson <50464822+helmersson@users.noreply.github.com>
2024-10-15 13:39:25 +0200

    Commented all code

--- src/Submission/InputOutput.java
+++ src/Submission/InputOutput.java
@@ -2,17 +2,24 @@ package Submission;
 import java.util.Scanner;
 
 public class InputOutput {
+    //  Skapar ett object för logic klassen
     private Logic logic = new Logic();
 
+    //  Input från användare
     public void inputProcess(){
+        //  Scanner
         Scanner scanner = new Scanner(System.in);
         System.out.println("Skriv in din text här. Skriv stop för att avbryta");
 
         String input;
+        //  Loop som körs tills att ordet stop skrivs
         while (!(input = scanner.nextLine()).equalsIgnoreCase("stop")){
             logic.addLine(input);
         }
+
+        //  Stänger scannern för att spara resurser
         scanner.close();
+        //  Skriver ut resultat när loopen avslutas
         logic.printResult();
     }
 
--- src/Submission/Logic.java
+++ src/Submission/Logic.java
@@ -2,24 +2,31 @@ package Submission;
 
 
 public class Logic {
-       private int charCount = 0;
-       private int lineCount = 0;
-       private int  wordCount = 0;
-       private String longestWord = "";
+    //Init på olika variablar som behövs
+    private int charCount = 0;
+    private int lineCount = 0;
+    private int  wordCount = 0;
+    private String longestWord = "";
 
+    //  Uppdaterar variablarna efter varje input
         public void addLine(String line) {
             lineCount++;
             charCount+= line.length();
 
+            //  Delar upp orden med hjälp med mellanslag
             String [] words = line.split("\\s+");
             wordCount += words.length;
 
+
+            //  Om det nya order är längre än det sparade längsta ordet
+            //  så uppdateras längsta ordet till senaste ordet
             for (String word : words) {
                 if (word.length () > longestWord.length()) {
                     longestWord = word;
                 }
             }
         }
+        //  Printar ut svaren i commadprompten
         public void printResult(){
             System.out.println("Antal rader:" + lineCount);
             System.out.println("Antal tecken:" + charCount);
