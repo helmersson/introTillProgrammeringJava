e6ffcbb382a8eef94c8b2d2d1bcf840ab3f9df0d
helmersson <50464822+helmersson@users.noreply.github.com>
2024-10-15 13:26:15 +0200

    Del 3

--- src/Submission/Logic.java
+++ src/Submission/Logic.java
@@ -2,16 +2,29 @@ package Submission;
 
 
 public class Logic {
-       private  int charCount = 0;
+       private int charCount = 0;
        private int lineCount = 0;
+       private int  wordCount = 0;
+       private String longestWord = "";
 
         public void addLine(String line) {
             lineCount++;
             charCount+= line.length();
+
+            String [] words = line.split("\\s+");
+            wordCount += words.length;
+
+            for (String word : words) {
+                if (word.length () > longestWord.length()) {
+                    longestWord = word;
+                }
+            }
         }
         public void printResult(){
             System.out.println("Antal rader:" + lineCount);
             System.out.println("Antal tecken:" + charCount);
+            System.out.println("Antal ord:" + wordCount);
+            System.out.println("Längsta ord:" + longestWord);
         }
 
 }
