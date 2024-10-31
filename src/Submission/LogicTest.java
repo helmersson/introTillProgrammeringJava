775adba1b191a695855730e81d3ad1d4e5163012
helmersson <50464822+helmersson@users.noreply.github.com>
2024-10-31 11:39:13 +0100

    Klar med submission behöver bara kommentera kod

--- src/Submission/LogicTest.java
+++ src/Submission/LogicTest.java
@@ -1,17 +1,78 @@
 package Submission;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LogicTest {
+
     @Test
-    void testLineCount() {
+    public void testCharCount() {
         Logic logicTest = new Logic();
+
         logicTest.addLine("Hej på dig");
         logicTest.addLine("Detta är en mening");
 
-        assertEquals(2, logicTest.getLineCount(), "antal rader ska vara två");
-        assertEquals(38, logicTest.getCharCount(), "antal tecken ska vara 30");
-        assertEquals(7, logicTest.getWordCount(), "antal ord ska vara 7");
+        int expected = 28;
+        int actual = logicTest.getCharCount();
+
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void testLineCount() {
+        Logic logicTest = new Logic();
+
+        logicTest.addLine("Hej på dig");
+        logicTest.addLine("Detta är en mening");
+
+        int expected = 2;
+        int actual = logicTest.getLineCount();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWordCount() {
+        Logic logicTest = new Logic();
+
+        logicTest.addLine("Hej på dig");
+        logicTest.addLine("Detta är en mening");
+
+        int expected = 7;
+        int actual = logicTest.getWordCount();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testLongestWord() {
+        Logic logicTest = new Logic();
+
+        logicTest.addLine("Hej på dig");
+        logicTest.addLine("hejhopp");
+
+        String expected = "hejhopp";
+        String actual = logicTest.getLongestWord();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testStopTrue() {
+        Logic logicTest = new Logic();
+        String[] differentStops = {"stop", "STOP", "sTOP", "StOp"};
+
+        for (String different : differentStops) {
+            assertTrue(logicTest.addLine(different));
+        }
+    }
+
+    @Test
+    public void testStopFalse() {
+        Logic logicTest = new Logic();
+        String[] differentStops = {"hej", "då", "ettmycketlångtord", "jaggillarinterepetetivkod"};
+
+        for (String different : differentStops) {
+            assertFalse(logicTest.addLine(different));
+        }
+    }
 }
